0236dd4217df5310d70b69e553266804e2f8b0d8
Jesse David Peterson <jesdavpet@users.noreply.github.com>
2026-03-03 10:10:21 -0400

    chore(tests): rename Mesh to Graphviz

--- tests/panel.spec.ts
+++ tests/panel.spec.ts
@@ -1,10 +1,10 @@
 import { test, expect } from '@grafana/plugin-e2e';
 
-test('should render mesh panel with DOT diagram', async ({ gotoPanelEditPage, readProvisionedDashboard, page }) => {
+test('should render graphviz panel with DOT diagram', async ({ gotoPanelEditPage, readProvisionedDashboard, page }) => {
   const dashboard = await readProvisionedDashboard({ fileName: 'dashboard.json' });
   const panelEditPage = await gotoPanelEditPage({ dashboard, id: '1' });
 
   await expect(panelEditPage.panel.locator).toBeVisible();
-  await expect(page.getByTestId('mesh-panel')).toBeVisible();
-  await expect(page.getByTestId('mesh-panel-svg').locator('svg')).toBeVisible();
+  await expect(page.getByTestId('graphviz-panel')).toBeVisible();
+  await expect(page.getByTestId('graphviz-panel-svg').locator('svg')).toBeVisible();
 });
